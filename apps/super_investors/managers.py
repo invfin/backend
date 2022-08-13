165e80f49111e6d9957f58fb65fc9a6eb5b6760f
lucas <lluc23@hotmail.com>
2022-08-13 12:17:35 +0200

    fix managers superinvestors current period

--- apps/super_investors/managers.py
+++ apps/super_investors/managers.py
@@ -5,7 +5,7 @@ class SuperinvestorManager(Manager):
     def current_positions(self, pk):
         from apps.super_investors.models import Period
         current_period = Period.objects.earliest()
-        return self.get(id=pk).history.filter(period_related=self.current_period)
+        return self.get(id=pk).history.filter(period_related=current_period)
 
     def resume_current_positions(self):
         pass
@@ -21,4 +21,4 @@ class SuperinvestorHistoryManager(Manager):
     def company_in_current_portfolios(self, company_id):
         from apps.super_investors.models import Period
         current_period = Period.objects.earliest()
-        return self.filter(period_related=self.current_period, company_id=company_id)
+        return self.filter(period_related=current_period, company_id=company_id)
