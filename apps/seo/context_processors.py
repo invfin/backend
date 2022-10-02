ac864382e012682c4088216e912692e97db5eb54
Lucas <lluc23@hotmail.com>
2022-10-02 19:19:15 +0200

    fix visiteurs

--- apps/seo/context_processors.py
+++ apps/seo/context_processors.py
@@ -1,10 +1,9 @@
-from django.contrib.auth import get_user_model
-
-from .outils.save_journey import JourneyClassifier
+from apps.api.models import Key
+from apps.seo.outils.save_journey import JourneyClassifier
 
 
 def journey(request):
-    if type(request.user) == get_user_model():
+    if type(request.user) != Key:
         current_path = request.build_absolute_uri()
         comes_from = str(request.META.get("HTTP_REFERER"))
         if request.user.is_authenticated:
