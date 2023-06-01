6ed2930df3ee163832026ab1a0a43e7339c65793
lucas <lluc23@hotmail.com>
2023-06-01 14:26:39 +0200

    move tests

---
+++ tests/test_empresas/test_outils/test_data_management/test_interfaces.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from src.empresas.outils.data_management.interfaces import (
+    AveragesInterface,
+    CompanyInterface,
+    StatementsInterface,
+)
+
+
+class TestAveragesInterface(TestCase):
+    def test_X(self):
+        assert 1 == 2
+
+
+class TestCompanyInterface(TestCase):
+    def test_X(self):
+        assert 1 == 2
+
+
+class TestStatementsInterface(TestCase):
+    def test_X(self):
+        assert 1 == 2
